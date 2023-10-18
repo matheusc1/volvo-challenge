098e26d427b50dbe2e2bb8971e551d0f500cdfcf
Matheus Cardoso <cardoso.matheusbs@gmail.com>
2023-10-18 20:16:10 -0300

    Add env variable

--- src/hooks/useCars.ts
+++ src/hooks/useCars.ts
@@ -6,7 +6,7 @@ export function useCars() {
   const [cars, setCars] = useState<Car[]>([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3000/api").then(res => {
+    axios.get(process.env.NEXT_PUBLIC_API_URL + "/api").then(res => {
       setCars(res.data)
     })
   }, [])
